044b5ae17296d601fa60229f5499ab4728b171f3
fernandoaguiar95 <62913150+fernandoaguiar95@users.noreply.github.com>
2020-08-28 11:37:21 -0300

    Add files via upload

---
+++ CadastroAlunos/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace CadastroAlunos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbData = new System.Windows.Forms.TextBox();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.tbNome = new System.Windows.Forms.TextBox();
+            this.btnSaveAluno = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(161, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CADASTRO DE ALUNOS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nome:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(87, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Data de Nascimento:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(87, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "E-mail:";
+            // 
+            // tbData
+            // 
+            this.tbData.Location = new System.Drawing.Point(200, 137);
+            this.tbData.Name = "tbData";
+            this.tbData.Size = new System.Drawing.Size(100, 20);
+            this.tbData.TabIndex = 5;
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Location = new System.Drawing.Point(200, 111);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(188, 20);
+            this.tbEmail.TabIndex = 6;
+            // 
+            // tbNome
+            // 
+            this.tbNome.Location = new System.Drawing.Point(200, 83);
+            this.tbNome.Name = "tbNome";
+            this.tbNome.Size = new System.Drawing.Size(188, 20);
+            this.tbNome.TabIndex = 7;
+            // 
+            // btnSaveAluno
+            // 
+            this.btnSaveAluno.Location = new System.Drawing.Point(178, 190);
+            this.btnSaveAluno.Name = "btnSaveAluno";
+            this.btnSaveAluno.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveAluno.TabIndex = 8;
+            this.btnSaveAluno.Text = "Salvar";
+            this.btnSaveAluno.UseVisualStyleBackColor = true;
+            this.btnSaveAluno.Click += new System.EventHandler(this.btnSaveAluno_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 260);
+            this.Controls.Add(this.btnSaveAluno);
+            this.Controls.Add(this.tbNome);
+            this.Controls.Add(this.tbEmail);
+            this.Controls.Add(this.tbData);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbData;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.TextBox tbNome;
+        private System.Windows.Forms.Button btnSaveAluno;
+    }
+}
+
---
+++ CadastroAlunos/Form1.cs
@@ -0,0 +1,67 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CadastroAlunos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSaveAluno_Click(object sender, EventArgs e)
+        {
+            MySqlConnectionStringBuilder conexaodb = new MySqlConnectionStringBuilder();
+            conexaodb.Server = "localhost";
+            conexaodb.Database = "faculdade";
+            conexaodb.UserID = "root";
+            conexaodb.Password = "";
+
+            MySqlConnection connectionString = new MySqlConnection(conexaodb.ToString());
+
+            try
+            {
+                connectionString.Open();
+                MessageBox.Show("Conexão aberta");
+
+                MySqlCommand cmd = connectionString.CreateCommand();
+                cmd.CommandText = "INSERT INTO alunos(nome, email, data_nascimento) VALUES ('" + tbNome.Text + "', '"
+                                    + tbEmail.Text + "', '" + tbData.Text + "')";
+                cmd.ExecuteNonQuery();
+
+                connectionString.Close();
+                MessageBox.Show("Aluno adicionado com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Não foi possível conectar!");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
